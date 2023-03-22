a3c8c75abfd018ab33f5fef6eb0e60a2ea700f32
Aleksey-Dr <aprel181818@gmail.com>
2023-03-22 22:41:05 +0200

    Update 01-galary.js
    Added import SimpleLightb:
    1) import SimpleLightbox from "simplelightbox";
    2) import "simplelightbox/dist/simple-lightbox.min.css";
    Added JavaScript code from the homework number seven (Task 2).

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+gallery.remove();
+const paragraph = document.querySelector('p');
+const galleryNew = document.createElement('div');
+paragraph.after(galleryNew);
+galleryNew.classList.add('gallery');
+
+createGalleryItems();
+
+function createGalleryItems() {
+    const items = [];
+
+    for (let i = 0; i < galleryItems.length; i++) {
+
+        const item = document.createElement('a');
+        item.href = galleryItems[i].original;
+        item.classList.add('gallery__item');
+
+        const image = document.createElement('img');
+        item.append(image);
+        image.src = galleryItems[i].preview;
+        image.alt = galleryItems[i].description;
+        image.classList.add('gallery__image');
+
+        items.push(item);
+    }
+
+    galleryNew.append(...items);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
