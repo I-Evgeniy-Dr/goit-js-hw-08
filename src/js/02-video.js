7bcc44276b51922a09a079f22068f80330cce140
Aleksey-Dr <aprel181818@gmail.com>
2023-03-25 11:15:51 +0200

    the solution of the task 2
    Add  @vimeo/player:
    npm install @vimeo/player;
    Add lodash.throttle
    npm install lodash.throttle;
    The solution of the Task 2.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+localStorageValue();
+// console.log('Hello!');
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const currentTimePlayed = throttle(function ({ seconds, }) {
+        localStorage.setItem(STORAGE_KEY, seconds);
+        // console.log('Checked throttle')
+    }, 1000);
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+player.on('timeupdate', currentTimePlayed);
+
+function localStorageValue() {
+    
+    if (localStorage.getItem(STORAGE_KEY)) {
+        player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+        // console.log('Checked condition true');
+    // } else {
+    //     console.log('Checked condition false');
+    }
+}
